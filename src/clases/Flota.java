df730f1c21f219cc1390e2b5562b16ec9eb89e25
esthermaroto <esther.maroto.torres@gmail.com>
2025-02-03 09:30:00 +0100

    estructura flota

--- src/clases/Flota.java
+++ src/clases/Flota.java
@@ -1,4 +1,34 @@
 package clases;
 
+import java.util.List;
+
 public class Flota {
+
+    private List<Barco> barquitos;
+
+
+    public Flota(List<Barco> barquitos) {
+        throw new UnsupportedOperationException("Sin programar");
+    }
+    public void añadirBarco(Barco b) {
+        throw new UnsupportedOperationException("Sin programar");
+    }
+    public int getNumeroBarcos() {
+        throw new UnsupportedOperationException("Sin programar");
+    }
+    public int getNumeroBarcosActivos() {
+        throw new UnsupportedOperationException("Sin programar");
+    }
+    public int getNumeroBarcosHumdidos() {
+        throw new UnsupportedOperationException("Sin programar");
+    }
+    public boolean todosBarcosHundidos() {
+        throw new UnsupportedOperationException("Sin programar");
+    }
+    public boolean hayBarco(int f, int c) {
+        throw new UnsupportedOperationException("Sin programar");
+    }
+    public int recibirDisparo(int f, int c) {
+        throw new UnsupportedOperationException("Sin programar");
+    }
 }
