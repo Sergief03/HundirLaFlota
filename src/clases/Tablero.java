d422933b5419de3db84236994174c62b3f52afd7
JuanMedina05 <juanperezmedina2005@gmail.com>
2025-02-03 09:22:53 +0100

    Tablero

--- src/clases/Tablero.java
+++ src/clases/Tablero.java
@@ -1,4 +1,31 @@
 package clases;
 
 public class Tablero {
+    private int totalFilas;
+    private int totalColumnas;
+    private Flota flota;
+    public Tablero(int f, int c){
+        throw new UnsupportedOperationException("Sin programar");
+    }
+    public Tablero(int n){
+        throw new UnsupportedOperationException("Sin programar");
+    }
+    public Tablero(){
+        throw new UnsupportedOperationException("Sin programar");
+    }
+    public int getTotalFilas(){
+        throw new UnsupportedOperationException("Sin programar");
+    }
+    public int getTotalColumnas(){
+        throw new UnsupportedOperationException("Sin programar");
+    }
+    public Flota getFlota(){
+        throw new UnsupportedOperationException("Sin programar");
+    }
+    public char getEstado(int f, int c){
+        throw new UnsupportedOperationException("Sin programar");
+    }
+    public void dibujar(boolean mostrarBarcos){
+        throw new UnsupportedOperationException("Sin programar");
+    }
 }
