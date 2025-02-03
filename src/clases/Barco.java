cede12d0f9e1b20d82381a2b14d301fcb1243619
Sergief03 <sergioandrespacogmail.com>
2025-02-03 10:12:27 +0100

    Programar barcp

--- src/clases/Barco.java
+++ src/clases/Barco.java
@@ -7,35 +7,97 @@ public class Barco {
     private boolean[] daño;
 
     public Barco(int f, int c, boolean v, int tam){
-        throw new UnsupportedOperationException("Sin programar");
+        this.fila=f;
+        this.columna=c;
+        this.vertical=v;
+        this.daño=new boolean[tam];
+
     }
 
     public int getFila(){
-        throw new UnsupportedOperationException("Sin programar");
+        return this.fila;
     }
 
     public int getColumna(){
-        throw new UnsupportedOperationException("Sin programar");
+        return this.columna;
     }
 
     public int getTamaño(){
-        throw new UnsupportedOperationException("Sin programar");
+        return this.daño.length;
     }
 
     public boolean estaHundido(){
-        throw new UnsupportedOperationException("Sin programar");
+        boolean hundido=false;
+        for (Boolean e:daño){
+            if (e==false){
+                hundido=false;
+            }
+        }
+        return hundido;
     }
 
-    public boolean hayBarcp(){
-        throw new UnsupportedOperationException("Sin programar");
+    public boolean hayBarcp(int f,int c){
+        boolean barco=false;
+        if (vertical){
+            if (c==this.columna||(c>=this.columna&&c<=this.daño.length)){
+                barco=true;
+            }
+        }else {
+            if (f==this.fila||(f>=this.fila&&c<=this.daño.length)){
+                barco=true;
+            }
+        }
+
+
+        return barco;
     }
 
     public int recibirDisparo(int f, int c){
-        throw new UnsupportedOperationException("Sin programar");
+        if (hayBarcp(f,c)){
+            if (vertical){
+                this.daño[(c-this.columna)]=true;
+            }else {
+                this.daño[(f-this.fila)]=true;
+            }
+
+        }else {
+            return -1;
+        }
+
+        if (estaHundido()){
+            return 2;
+        }else {
+            return 1;
+        }
     }
 
-    public int getEstado(){
-        throw new UnsupportedOperationException("Sin programar");
+    public int getEstado(int f, int c){
+        if (hayBarcp(f,c)){
+            if (vertical){
+                if (this.daño[(c-this.columna)]==true){
+                    if (estaHundido()){
+                        return 2;
+                    }else {
+                        return 1;
+                    }
+                }else {
+                    return 0;
+                }
+
+            }else {
+                if (this.daño[(f-this.fila)]==true){
+                    if (estaHundido()){
+                        return 2;
+                    }else {
+                        return 1;
+                    }
+                }else {
+                    return 0;
+                }
+            }
+        }else {
+            return -1;
+        }
     }
 }
 
